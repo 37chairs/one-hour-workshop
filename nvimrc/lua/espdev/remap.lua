e1d1f1480963b7509a830fdfc083d79353f88654
Bryan Thompson <bryan@37chairs.com>
2025-02-09 09:28:42 -0600

    chore: updating nvim + fugitive maps

--- nvimrc/lua/espdev/remap.lua
+++ nvimrc/lua/espdev/remap.lua
@@ -9,9 +9,23 @@ vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fin
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "List Buffers" })
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git) -- Open git status (vim-fugitive)
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>") -- Quickly quit a buffer
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>") -- Quickly save a file
+
+-- Open Fugitive Git Status
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git Status (Fugitive)" })
+
+-- Git Add and Commit
+vim.keymap.set("n", "<leader>ga", ":Gwrite<CR>", { desc = "Git Add (Stage Current File)" })
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git Commit" })
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git Push" })
+
+-- Git Diff
+vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { desc = "Git Diff" })
+
+-- Git Reset Current File
+vim.keymap.set("n", "<leader>gr", ":Gread<CR>", { desc = "Git Reset (Revert File)" })
+
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
